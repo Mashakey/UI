11c5d8169d6c03cc89b1506b0edb650d12408479
Tasty258 <marysya753@mail.ru>
2020-06-10 23:03:35 +0300

    init commit 1

---
+++ 1/Assets/Scripts/Nazadstr.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Nazadstr : MonoBehaviour
+{
+	public void ChangeScene(string sceneName)
+	{
+		SceneManager.LoadScene(0);
+	}
+	public void Exit()
+	{
+		Application.Quit();
+	}
+}
